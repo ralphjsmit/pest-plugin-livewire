47eac963a8effb9e9afcacd9d745100ce34ba688
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-01-11 16:07:02 +0100

    Delete dump statements

--- src/Expectations.php
+++ src/Expectations.php
@@ -8,15 +8,11 @@ use Pest\Expectation;
 expect()->extend('toHaveRequiredProperties', function (Collection $validInput, array $properties, string $submit): Expectation {
     assertRequiredProperties($this->value, $validInput, $properties, $submit);
 
-    dump(get_class($this));
-
     return $this;
 });
 
 expect()->extend('toNotHaveRequiredProperties', function (Collection $validInput, array $properties, string $submit): Expectation {
     assertNotRequiredProperties($this->value, $validInput, $properties, $submit);
-
-    dump(get_class($this->value));
 
     return $this;
 });
